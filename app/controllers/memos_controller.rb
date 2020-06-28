c569e7a82b02912700ccbec3409583ff4739f6a3
EC2 Default User <ec2-user@ip-172-31-55-189.ec2.internal>
2020-06-28 12:08:42 +0000

    add delete and edit

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -10,4 +10,18 @@ class MemosController < ApplicationController
       Memo.create(title:params["memos"]["title"], body:params["memos"]["body"])
       redirect_to "/"
   end
+  def destroy
+      memo=Memo.find(params["id"])
+      memo.destroy
+      redirect_to "/"
+  end
+  def edit
+      @memo=Memo.find(params["id"])
+  end
+  def update
+      memo=Memo.find(params["id"])
+      memo.update(title:params["memos"]["title"], body:params["memos"]["body"])
+      memo.save
+      redirect_to "/"
+  end
 end
